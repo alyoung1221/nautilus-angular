32e0208abd21386f972e3a105665cf8e8895bea4
Alana <alana3lan@gmail.com>
2021-02-07 21:32:42 -0500

    Created initial reservation page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ContactComponent } from './components/contact/contact.component';
+import { ReservationComponent } from './components/reservation/reservation.component';
 
 const routes: Routes = [
-  { path: 'contact', component: ContactComponent }
+  { path: 'contact', component: ContactComponent },
+  { path: 'reservations', component: ReservationComponent }
 ];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
