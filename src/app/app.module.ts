52ed9fe78e965160d9a21a76db45bc4853b16fe2
Alana <alana3lan@gmail.com>
2021-02-17 21:15:37 -0500

    Installed ngx-number-spinner

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { FlatpickrModule } from 'angularx-flatpickr';
+import { NgxNumberSpinnerModule } from 'ngx-number-spinner';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
@@ -10,7 +11,6 @@ import { AddressComponent } from './includes/address/address.component';
 import { FooterComponent } from './includes/footer/footer.component';
 import { ContactComponent } from './components/contact/contact.component';
 import { ReservationComponent } from './components/reservation/reservation.component';
-
 
 @NgModule({
   declarations: [
@@ -25,7 +25,8 @@ import { ReservationComponent } from './components/reservation/reservation.compo
     BrowserModule,
     AppRoutingModule,
     FormsModule, 
-    FlatpickrModule.forRoot(),
+    FlatpickrModule.forRoot(),    
+    NgxNumberSpinnerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
